4af25752abdf0f045d18563feacf0fee22e06baa
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-09-19 13:37:42 +0600

    CSS intigration in react

--- React-App/src/index.jsx
+++ React-App/src/index.jsx
@@ -1,4 +1,5 @@
 import { createRoot } from 'react-dom/client'
+
 const todoTitle="Call Family";
 const todoDes="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, maxime.";
 const monthName=new Date().getMonth();
@@ -6,10 +7,11 @@ const dateName=new Date().getDate()
 const currentYear=new Date().getFullYear();
 
 
+
 createRoot(document.getElementById('root')).render(
 
    <div>
-     <h1>Todo App</h1>
+     <h1 className='headinStyle'>Todo App</h1>
         <h3>{todoTitle}</h3>
         <p>{todoDes}</p>
         <p>{dateName+"/"+monthName+"/"+currentYear}</p>
