ca7f42ef7faac230cecdefb1f80c95230a6b0caf
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-09-19 13:19:10 +0600

    React Installation and file structure

---
+++ React-App/src/index.jsx
@@ -0,0 +1,18 @@
+import { createRoot } from 'react-dom/client'
+const todoTitle="Call Family";
+const todoDes="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod, maxime.";
+const monthName=new Date().getMonth();
+const dateName=new Date().getDate()
+const currentYear=new Date().getFullYear();
+
+
+createRoot(document.getElementById('root')).render(
+
+   <div>
+     <h1>Todo App</h1>
+        <h3>{todoTitle}</h3>
+        <p>{todoDes}</p>
+        <p>{dateName+"/"+monthName+"/"+currentYear}</p>
+   </div>
+ ,
+)
